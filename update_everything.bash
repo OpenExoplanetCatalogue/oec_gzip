26c41701199585d120399dcb414f2c09b53f1942
oec-bot-webhook <webhook@openexoplanetcataloque.com>
2021-01-02 00:01:01 +0100

    No timestamp info in gzip

--- update_everything.bash
+++ update_everything.bash
@@ -4,7 +4,7 @@ rm $FILENAME.gz
 echo "<systems>" > $FILENAME
 cat open_exoplanet_catalogue/systems/*.xml >> $FILENAME
 echo "</systems>" >> $FILENAME
-gzip $FILENAME 
+gzip -n $FILENAME 
 git add $FILENAME.gz
 git commit -a -m "Automatic update (via webhook)."
 git push
