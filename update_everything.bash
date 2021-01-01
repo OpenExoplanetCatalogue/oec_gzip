4d38b81f1a99a054f37763195881baf174f359d8
oec-bot-webhook <webhook@openexoplanetcataloque.com>
2021-01-01 23:56:05 +0100

    typo

--- update_everything.bash
+++ update_everything.bash
@@ -2,7 +2,7 @@
 FILENAME="systems.xml"
 rm $FILENAME.gz
 echo "<systems>" > $FILENAME
-cat open_exoplanet_catalogue-master/systems/*.xml >> $FILENAME
+cat open_exoplanet_catalogue/systems/*.xml >> $FILENAME
 echo "</systems>" >> $FILENAME
 gzip $FILENAME 
 git add $FILENAME.gz
