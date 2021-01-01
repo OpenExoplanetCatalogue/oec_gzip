3c6fe3187ae94f40d13a7ac7b9e2384a2e9d3d9c
oec-bot-webhook <webhook@openexoplanetcataloque.com>
2021-01-01 23:55:12 +0100

    using local version of oec

--- update_everything.bash
+++ update_everything.bash
@@ -1,18 +1,10 @@
 #!/bin/bash
-mkdir tmp
-pushd tmp
-wget --no-check-certificate https://github.com/OpenExoplanetCatalogue/open_exoplanet_catalogue/archive/master.zip
-unzip master
-popd
-
 FILENAME="systems.xml"
 rm $FILENAME.gz
 echo "<systems>" > $FILENAME
-cat tmp/open_exoplanet_catalogue-master/systems/*.xml >> $FILENAME
+cat open_exoplanet_catalogue-master/systems/*.xml >> $FILENAME
 echo "</systems>" >> $FILENAME
 gzip $FILENAME 
 git add $FILENAME.gz
 git commit -a -m "Automatic update (via webhook)."
 git push
-
-rm -rf tmp
