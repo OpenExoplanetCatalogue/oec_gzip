7fdcb6b7a7cd9c851fefad966e47e89319f61c98
oec-bot-webhook <webhook@openexoplanetcataloque.com>
2022-08-05 18:20:20 +0200

    Automatic update oec (via webhook).

--- update_everything.bash
+++ update_everything.bash
@@ -4,7 +4,27 @@ rm $FILENAME.gz
 echo "<systems>" > $FILENAME
 cat open_exoplanet_catalogue/systems/*.xml >> $FILENAME
 echo "</systems>" >> $FILENAME
-gzip -n $FILENAME 
+gzip -n $FILENAME
 git add $FILENAME.gz
-git commit -a -m "Automatic update (via webhook)."
+git commit -a -m "Automatic update oec (via webhook)."
+git push
+
+FILENAME="systems-exoplaneteu.xml"
+rm $FILENAME.gz
+echo "<systems>" > $FILENAME
+cat open_exoplanet_catalogue/systems_exoplaneteu/*.xml >> $FILENAME
+echo "</systems>" >> $FILENAME
+gzip -n $FILENAME
+git add $FILENAME.gz
+git commit -a -m "Automatic update oec_exoplaneteu (via webhook)."
+git push
+
+FILENAME="systems-exoplanetarchive.xml"
+rm $FILENAME.gz
+echo "<systems>" > $FILENAME
+cat open_exoplanet_catalogue/systems_exoplanetarchive/*.xml >> $FILENAME
+echo "</systems>" >> $FILENAME
+gzip -n $FILENAME
+git add $FILENAME.gz
+git commit -a -m "Automatic update oec_exoplanetarchive (via webhook)."
 git push
